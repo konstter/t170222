bb507537e573c7b66436163181342c40ac1e9e5b
konstter <konst.br@gmail.com>
2022-02-18 09:19:58 +0800

    First version

--- answer.py
+++ answer.py
@@ -2,23 +2,39 @@ import json
 
 
 def read_file(file):
+    '''Получаем структуру из json в список-дано'''
     with open(file, 'r') as f:
         json_input = f.read()
         p = json.loads(json_input)
-        struct_to_list(p, [])
+        print(struct_to_list(p))
+        return p
 
 
-def struct_to_list(paths, path_list):
-    for path in paths:
-        path_list.append(path['name'])
-        if path['sub_folders']:
+def struct_to_list(ps):
+    '''Рекурсивно обходим список-дано, на каждой итерации
+    запоминаем путь папок в новый список-путь. Если в папке
+    текущей итерации есть файлы, записываем каждый файл и его 
+    список-путь в кортеж и присоединяем кортеж к списку-результату.
+    Если подпапок нет - удаляем текущую папку из списка-пути'''
+    result = []
+
+    def rec(paths, path_list, result):
+        for path in paths:
+            path_list.append(path['name'])
+
             if path['files']:
-                print(path['files'], '|', path_list)
-            struct_to_list(path['sub_folders'], path_list)
-        else:
-            print(path['files'], '|', path_list)
+                for p_f in path['files']:
+                    elem = (p_f, list(path_list[:]))
+                    result.append(elem)
 
-       path_list = []
+            if path['sub_folders']:
+                rec(path['sub_folders'], path_list, result)
+
+            path_list.pop()
+
+    rec(ps, [], result)
+
+    return result
 
 
 if __name__ == "__main__":
